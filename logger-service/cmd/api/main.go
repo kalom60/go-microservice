42104adc65238e8955e61f58d7c50b12ed9c7fea
kalom60 <39136564+kalom60@users.noreply.github.com>
2024-08-02 16:59:58 +0300

    initializing rpc and registering

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"github.com/kalom60/go-microservice/logger/data"
@@ -47,6 +49,10 @@ func main() {
 		Models: data.New(client),
 	}
 
+	// Register the RPC server
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
+
 	// start web server
 	log.Println("Starting service on port", webPort)
 	srv := &http.Server{
@@ -57,6 +63,25 @@ func main() {
 	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port", rpcPort)
+
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return nil
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+
+		go rpc.ServeConn(rpcConn)
 	}
 }
 
